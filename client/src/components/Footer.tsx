d757ab5bac5897471bfd42efa275f099a69d3fc4
deymohit02 <deymohit5@gmail.com>
2026-02-04 19:19:22 +0530

    Added a new Footer component with social links and copyright information

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -10,9 +10,9 @@ const socialLinks = [
 
 export default function Footer() {
   return (
-    <footer className="bg-background border-t border-border py-12">
+    <footer className="bg-background border-t border-border py-6">
       <div className="max-w-7xl mx-auto px-6 md:px-8">
-        <div className="flex flex-col items-center gap-8">
+        <div className="flex flex-col items-center gap-4">
           <div className="flex gap-4">
             {socialLinks.map((social, index) => (
               <Button
